ce34f61f21ec580e6a282f6f70fa28ce7e35119b
John Samuel <johnsamuelwrites@gmail.com>
2020-04-25 13:10:14 +0200

    Add tool bar and menu options

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,49 @@
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  entry: './oddol.js'
+  entry: ['./oddol.scss', './oddol.js'],
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          {loader: 'extract-loader'},
+          {loader: 'css-loader'},
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+              sassOptions: {
+                includePaths: ['./node_modules'],
+              },
+            },
+          }
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+        },
+      }
+    ],
+  },
 };
+
