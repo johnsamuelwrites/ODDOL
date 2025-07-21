b49bd65269dacab6b71b2025f4ed4e81debf851f
John Samuel <johnsamuelwrites@gmail.com>
2025-07-21 17:29:56 +0200

    Update dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,9 @@ const autoprefixer = require('autoprefixer');
 const path = require('path');
 const BUILD_DIR = path.resolve(__dirname, './');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Add this line
+
 module.exports = {
   entry: ['./oddol.scss', './oddol.js'],
   output: {
@@ -9,56 +12,77 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html', // Path to your source index.html file
+      filename: 'index.html'    // Output filename in the BUILD_DIR
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'bundle.css',
-            },
-          },
-          {loader: 'extract-loader'},
-          {loader: 'css-loader'},
+          'style-loader', // Injects CSS into the DOM
+          'css-loader',   // Interprets @import and url() like import/require()
           {
             loader: 'postcss-loader',
             options: {
-              plugins: () => [autoprefixer()]
-            }
+              postcssOptions: {
+                plugins: [
+                  require('autoprefixer'), // Autoprefixer should be here
+                  // You can add other PostCSS plugins here, e.g., cssnano for minification in production
+                  // require('cssnano')({
+                  //   preset: 'default',
+                  // }),
+                ],
+              },
+              sourceMap: true, // Generate source maps for easier debugging
+            },
           },
           {
-            loader: 'sass-loader',
+            loader: 'sass-loader', // Compiles Sass to CSS
             options: {
-              // Prefer Dart Sass
-              implementation: require('sass'),
+              // This is crucial for resolving @material imports
               sassOptions: {
-                includePaths: ['./node_modules'],
+                includePaths: [
+                  path.resolve(__dirname, 'node_modules'), // Tell Sass to look in node_modules
+                ],
               },
+              sourceMap: true, // Generate source maps for Sass
             },
-          }
+          },
         ],
       },
       {
         test: /\.js$/,
+        exclude: /node_modules/, // Exclude node_modules for faster transpilation
         loader: 'babel-loader',
-        query: {
+        options: {
           presets: ['@babel/preset-env'],
         },
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
+        type: 'asset/resource', // Use Webpack 5's built-in asset modules
+        generator: {
+          filename: 'fonts/[name][ext]', // Output fonts to a 'fonts' directory
+        },
+      },
+      // You might also want a rule for images
+      {
+        test: /\.(png|jpe?g|gif|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
       },
     ],
   },
+  // Add a devtool for better source mapping in development
+  devtool: 'eval-source-map', // Or 'source-map' for production
 };
